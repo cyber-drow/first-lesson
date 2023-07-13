84b218b5296b8240f3611da1e3c25cb23f2cfc83
cyberdrow <cbr.drw@ya.ru>
2023-07-13 08:15:29 +0400

    add 4-navigator

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log("Hello World!")
+const positionLat = 10, positionLong = 34, addressLat = 3, addressLong = 10;
+const distance2 =  Math.sqrt(Math.pow((positionLong - positionLat), 2) + Math.pow((addressLong - addressLat), 2));
+
+console.log(`Расстояние между двумя точками равно ${distance2}`);
